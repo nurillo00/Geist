8451b61bf86e8ce92aa86e8fd1f6b465edf79e9e
nurillo00 <nurillobest33@gmail.com>
2024-05-02 15:09:28 +0500

    .

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
         <section className='hero'>
             <div className="hero-wrap">
                 <div className="hero-text-box">
-                    <h1 className='hero-title'><span className='hero-person'>My</span>Team 🧑‍💻😎</h1>
+                    <h1 className='hero-title'><span className='hero-person'>My</span> Team 🧑‍💻😎</h1>
                     <p className='hero-text'>A front-end engineer and UI/UX designer helping startups turn their visions into a digital reality. I specialize in designing and building modern mobile and web-based apps.</p>
                     <div className="btn-wrap">
                         <button className='btn1'>See my resume</button>
